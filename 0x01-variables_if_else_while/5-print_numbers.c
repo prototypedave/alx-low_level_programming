ad883a093f1de8e3adec4aa7b03b6db61e80ce00
prototypedave <popupgfj@gmail.com>
2021-08-27 13:36:17 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main- Entry point
+ *
+ *Return: 0 Always (Success)
+ */
+int main(void)
+{
+int n = 0;
+do {
+printf("%d\n", n);
+n++;
+} while (n < 10);
+return (0);
+}
