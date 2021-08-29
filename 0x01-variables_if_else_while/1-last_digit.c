4df77198eb74eef920c1a9369e11301d9cffe67c
prototypedave <popupgfj@gmail.com>
2021-08-28 22:41:21 -0700

    last digit in one sentence

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 lastD = n % 10;
 if (lastD > 5)
 {
-printf("Last digit of %d is %d\n and is greater than 5\n", n, lastD);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
 }
 else if (lastD == 0)
 {
-printf("Last digit of %d is %d\n and is 0\n", n, lastD);
+printf("Last digit of %d is %d and is 0\n", n, lastD);
 }
 else
 {
-printf("Last digit of %d is %d\n and is less than 6 and not 0\n", n, lastD);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
 }
 return (0);
 }
