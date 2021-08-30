0d7973a37240664c0c21e99b4d2d4d0175c7e216
prototypedave <popupgfj@gmail.com>
2021-08-30 13:55:00 -0700

    fixing errors

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,12 @@
 /**
 *main- Entry point
 *
-*Return: Always 0 (Success)
+*return: 0 Always (Success)
 */
 int main(void)
 {
 char x;
-for (x = '0'; x <= '9'; x++)
-{
-putchar(x);
-}
-for (x = 'A'; x <= 'F'; x++)
+for (x = 'A'; x <= 'Z'; x++)
 putchar(tolower(x));
-putchar('\n');
 return (0);
 }
