f5ae522625f61568916a986ac46db6d25f9233b5
prototypedave <popupgfj@gmail.com>
2021-09-02 11:03:27 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_numbers- numbers base10
+  *
+ *Return: void
+ */
+void print_numbers(void)
+{
+  char x = 48;
+  while (x < 58)
+{
+_putchar(x);
+ x++;
+}
+_putchar('\n');
+}
