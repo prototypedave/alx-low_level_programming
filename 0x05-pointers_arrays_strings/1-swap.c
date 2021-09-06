5d7f34306d851fc48eed095e1dd20cf0045032cf
prototypedave <popupgfj@gmail.com>
2021-09-05 19:02:18 -0700

    correction for betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
-/*
- * swap_int - main function
+/**
+ * swap_int - file function declaration
  * @a: parameter1
  * @b: parameter2
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
-int n, p;
+int n;
+int p;
 n = *a;
 p = *b;
 *a = p;
-*b = n;;
+*b = n;
 }
