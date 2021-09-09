f2885ea7a97854b66b7dccdb9d8c7b72b1358129
prototypedave <popupgfj@gmail.com>
2021-09-08 21:12:08 -0700

    betty for strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <string.h>
 /**
- *_strcat- function to concanate 
+ *_strcat- function to concanate
  *@dest: parameter 1
  *@src: parameter 2
  *
- *Return dest
+ *Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-char* ptr = dest + strlen(dest);
-while (*src != '\0') {
+char *ptr = dest + strlen(dest);
+while (*src != '\0')
 *ptr++ = *src++;
-}
 *ptr = '\0';
-return dest;
+return (dest);
 }
