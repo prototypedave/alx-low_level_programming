fc00f404bbfd02ce29a03e283e8a75e3a55af73f
prototypedave <popupgfj@gmail.com>
2021-09-12 21:33:16 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn- function name
+ * @s: parameter 1
+ * @accept: parameter 2
+ *
+ * Return: i
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+break;
+}
+if (!accept[j])
+break;
+}
+return (i);
+}
