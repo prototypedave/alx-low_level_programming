63be7bec3a4cfd83e306910e6eba9b431ff6d1a0
prototypedave <popupgfj@gmail.com>
2021-09-21 09:43:29 -0700

    directory and task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array- function nam creates array of chars
+ * @size: parameter for size of array to be created
+ * @c: char to iniyialize array with
+ *
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *a;
+if (size == 0)
+return (NULL);
+a = malloc(sizeof(char) * size);
+if (a == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+a[i] = 0;
+return (a);
+}
